9d3c384805e26cbe4a6cdad6ddcf674f30420599
Gleb <kgm20011505@mail.ru>
2022-04-27 22:30:26 +0300

    header is done

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
 
+    fetch("./html/header.html") //adding HTML page (header) in general page (index)
+        .then(response => {
+            return response.text()
+        })
+        .then(data => {
+            document.querySelector("header").innerHTML = data;
+    });
 })
